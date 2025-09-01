7b1d0962227ec49b942990f537cac4fd2d2384f4
xMohnad <mohnad00967@gmail.com>
2025-09-01 05:34:16 +0000

    feat(lsp): add per-server configurations for lua_ls and pyright

---
+++ lsp/lua_ls.lua
@@ -0,0 +1,13 @@
+return {
+	cmd = { "lua-language-server" },
+	filetypes = "lua",
+	Lua = {
+		runtime = { version = "LuaJIT" },
+		workspace = {
+			library = {
+				vim.fn.expand("$VIMRUNTIME/lua"),
+				"${3rd}/luv/library",
+			},
+		},
+	},
+}
