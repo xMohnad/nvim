a426a525b16f2ff6b0f83258e8c7f61f662f6f69
xMohnad <mohnad00967@gmail.com>
2025-09-01 04:28:07 +0000

    feat: set leader keys and load core modules

---
+++ init.lua
@@ -0,0 +1,7 @@
+vim.g.mapleader = " " -- Set leader key before Lazy
+vim.g.maplocalleader = " " -- Set <LocalLeader> key to Space
+
+require("options")
+require("Lazy")
+require("autocmds")
+require("mappings")
