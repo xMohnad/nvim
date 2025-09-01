af522a644a38876314a83b8e4812b5975de377ab
xMohnad <mohnad00967@gmail.com>
2025-09-01 07:38:28 +0000

    feat(plugins): add supermaven-nvim for AI suggestions in Insert mode

---
+++ lua/plugins/supermaven.lua
@@ -0,0 +1,15 @@
+return {
+  "supermaven-inc/supermaven-nvim",
+  event = "InsertEnter",
+  opts = {
+    keymaps = {
+      accept_suggestion = "<Tab>",
+      clear_suggestion = "<C-]>",
+      accept_word = "<C-j>",
+    },
+    color = {
+      suggestion_color = "#475284",
+      cterm = 1,
+    },
+  }
+}
