8bb86b38077fbe3ac140e2f8f8c725d8d610752b
xMohnad <mohnad00967@gmail.com>
2025-09-10 09:41:16 +0000

    refactor(lspconfig): move mason to separate file

--- lua/plugins/devtools/lspconfig.lua
+++ lua/plugins/devtools/lspconfig.lua
@@ -1,26 +1,5 @@
 return {
 	{
-		"mason-org/mason.nvim",
-		lazy = false, -- Load immediately to ensure PATH is set
-		cmd = "Mason",
-		build = ":MasonUpdate",
-		opts = {
-			ensure_installed = {
-				"black",
-				"ruff",
-				"prettier",
-				"stylua",
-			},
-			ui = {
-				icons = {
-					package_pending = " ",
-					package_installed = " ",
-					package_uninstalled = " ",
-				},
-			},
-		},
-	},
-	{
 		"mason-org/mason-lspconfig.nvim",
 		dependencies = { "neovim/nvim-lspconfig" },
 		opts = {
---
+++ lua/plugins/devtools/mason.lua
@@ -0,0 +1,21 @@
+return {
+	"mason-org/mason.nvim",
+	lazy = false, -- Load immediately to ensure PATH is set
+	cmd = "Mason",
+	build = ":MasonUpdate",
+	opts = {
+		ensure_installed = {
+			"black",
+			"ruff",
+			"prettier",
+			"stylua",
+		},
+		ui = {
+			icons = {
+				package_pending = " ",
+				package_installed = " ",
+				package_uninstalled = " ",
+			},
+		},
+	},
+}
