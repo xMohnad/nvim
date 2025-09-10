d0e6fbf63eece7b3a9f48c12baec0b550a65b815
xMohnad <mohnad00967@gmail.com>
2025-09-10 08:20:15 +0000

    fix: update vim.notify log level usage

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -57,7 +57,7 @@ autocmd("LspProgress", {
     end, p)
 
     local spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" }
-    vim.notify(table.concat(msg, "\n"), "info", {
+    vim.notify(table.concat(msg, "\n"), vim.log.levels.INFO, {
       id = "lsp_progress",
       title = client.name,
       opts = function(notif)
