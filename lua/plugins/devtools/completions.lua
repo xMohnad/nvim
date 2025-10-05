080e5e45a99310295a35d711b26090ce744c2d50
xMohnad <mohnad00967@gmail.com>
2025-10-05 12:27:28 +0000

    refactor(completions): clean up blink.cmp config and adjust sources & appearance

--- lua/plugins/devtools/completions.lua
+++ lua/plugins/devtools/completions.lua
@@ -1,38 +1,22 @@
 return {
 	{
 		"saghen/blink.cmp",
-		-- optional: provides snippets for the snippet source
 		dependencies = {
 			"rafamadriz/friendly-snippets",
-			"L3MON4D3/LuaSnip",
 			"xzbdmw/colorful-menu.nvim",
 		},
 
 		-- use a release tag to download pre-built binaries
 		version = "*",
-		-- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-		-- build = "cargo build --release",
-		-- If you use nix, you can build from source using latest nightly rust with:
-		-- build = 'nix run .#build-plugin',
 
 		---@module 'blink.cmp'
 		---@type blink.cmp.Config
 		opts = {
-			-- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-			-- 'super-tab' for mappings similar to vscode (tab to accept)
-			-- 'enter' for enter to accept
-			-- 'none' for no mappings
-			--
-			-- All presets have the following mappings:
-			-- C-space: Open menu or open docs if already open
-			-- C-n/C-p or Up/Down: Select next/previous item
-			-- C-e: Hide menu
-			-- C-k: Toggle signature help (if signature.enabled = true)
-			--
 			-- See :h blink-cmp-config-keymap for defining your own keymap
 			keymap = { preset = "enter" },
 
 			appearance = {
+				use_nvim_cmp_as_default = false,
 				-- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
 				-- Adjusts spacing to ensure icons are aligned
 				nerd_font_variant = "mono",
@@ -41,18 +25,13 @@ return {
 				trigger = {
 					show_on_trigger_character = true,
 
-					-- Optionally, set a list of characters that will not trigger the completion window,
-					-- even when sources request it. The following are the defaults:
 					show_on_blocked_trigger_characters = { " ", "\n", "\t" },
 				},
 				menu = {
-					border = "rounded", -- Defaults to `vim.o.winborder` on nvim 0.11+ (if nil)
+					border = nil,
 					scrollbar = false,
 
 					draw = {
-						padding = { 0, 1 },
-						-- We don't need label_description now because label and label_description are already
-						-- combined together in label by colorful-menu.nvim.
 						columns = { { "kind_icon" }, { "label", gap = 1 } },
 						components = {
 							label = {
@@ -68,7 +47,7 @@ return {
 				},
 				documentation = {
 					window = {
-						border = "rounded",
+						border = nil,
 						winhighlight = "Normal:BlinkCmpDoc,FloatBorder:BlinkCmpDocBorder,EndOfBuffer:BlinkCmpDoc",
 					},
 					auto_show = true,
@@ -77,30 +56,22 @@ return {
 				-- Display a preview of the selected item on the current line
 				ghost_text = { enabled = true },
 			},
-
-			signature = { window = { border = "rounded" } },
-
+			signature = { enabled = true },
 			-- Default list of enabled providers defined so that you can extend it
 			-- elsewhere in your config, without redefining it, due to `opts_extend`
 			sources = {
-				default = { "lsp", "path", "snippets", "buffer" },
+				default = { "lazydev", "lsp", "snippets", "path", "buffer" },
 				providers = {
 					lazydev = {
 						name = "LazyDev",
 						module = "lazydev.integrations.blink",
-						-- make lazydev completions top priority (see `:h blink.cmp`)
 						score_offset = 100,
 					},
 				},
 			},
-
-			-- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
-			-- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
-			-- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
-			--
-			-- See the fuzzy documentation for more information
 			fuzzy = { implementation = "prefer_rust_with_warning" },
 		},
+
 		opts_extend = { "sources.default" },
 	},
 
