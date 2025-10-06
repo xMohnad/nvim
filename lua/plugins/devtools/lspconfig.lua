97e1ed2c502e78a45166fd9c2ced2205475756a1
xMohnad <mohnad00967@gmail.com>
2025-10-06 03:18:45 +0000

    refactor(lsp): replace pyright with basedpyright

--- lua/plugins/devtools/lspconfig.lua
+++ lua/plugins/devtools/lspconfig.lua
@@ -3,7 +3,7 @@ return {
 		"mason-org/mason-lspconfig.nvim",
 		dependencies = { "neovim/nvim-lspconfig" },
 		opts = {
-			ensure_installed = { "lua_ls", "pyright" },
+			ensure_installed = { "lua_ls", "basedpyright" },
 		},
 	},
 	{
