97101f017927198e51d92806e6ca8058f83bb47a
xMohnad <mohnad00967@gmail.com>
2025-10-06 07:03:09 +0000

    refactor(devtools): move lazydev.nvim to completions

--- lua/plugins/devtools/completions.lua
+++ lua/plugins/devtools/completions.lua
@@ -80,4 +80,16 @@ return {
 		event = "InsertEnter",
 		config = true,
 	},
+
+	{
+		"folke/lazydev.nvim",
+		ft = "lua", -- only load on lua files
+		opts = {
+			library = {
+				-- See the configuration section for more details
+				-- Load luvit types when the `vim.uv` word is found
+				{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+			},
+		},
+	},
 }
--- lua/plugins/devtools/lspconfig.lua
+++ lua/plugins/devtools/lspconfig.lua
@@ -1,20 +1,7 @@
 return {
-	{
-		"mason-org/mason-lspconfig.nvim",
-		dependencies = { "neovim/nvim-lspconfig" },
-		opts = {
-			ensure_installed = { "lua_ls", "basedpyright" },
-		},
-	},
-	{
-		"folke/lazydev.nvim",
-		ft = "lua", -- only load on lua files
-		opts = {
-			library = {
-				-- See the configuration section for more details
-				-- Load luvit types when the `vim.uv` word is found
-				{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
-			},
-		},
+	"mason-org/mason-lspconfig.nvim",
+	dependencies = { "neovim/nvim-lspconfig" },
+	opts = {
+		ensure_installed = { "lua_ls", "basedpyright" },
 	},
 }
