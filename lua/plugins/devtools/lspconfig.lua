24a9a0f9703224e27f86dfeb697c18242f37de74
xMohnad <mohnad00967@gmail.com>
2025-10-09 14:28:21 +0000

    fix(lsp): correct fish LSP server name

--- lua/plugins/devtools/lspconfig.lua
+++ lua/plugins/devtools/lspconfig.lua
@@ -1,6 +1,6 @@
 return {
 	"neovim/nvim-lspconfig",
 	config = function()
-		vim.lsp.enable({ "lua_ls", "basedpyright", "fish-lsp"  })
+		vim.lsp.enable({ "lua_ls", "basedpyright", "fish_lsp"  })
 	end,
 }
